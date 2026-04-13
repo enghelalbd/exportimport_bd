3d9ab30ecfbce6bfc454f31bd5f12e7fdc211afc
enghelalbd <informhelal@yahoo.com>
2026-04-13 12:15:50 +0600

    Tailwindcss installed

---
+++ src/Component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>Copyright @ 2024 Helal foodbd</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex">
+      <h1> Helal foodbd</h1>
+      <div>
+        <h1>All Products</h1>
+        <h1>My Exports</h1>
+        <h1>My Imports</h1>
+      </div>
+
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default Navbar;
