e165c967b8399c72269e0cd58d2b4e90340f50e1
enghelalbd <informhelal@yahoo.com>
2026-04-15 14:51:47 +0600

    Navbar runnig

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,20 +1,17 @@
 import React from "react";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
       <button className="btn btn-primary">Logo</button>
-      <button className="btn btn-soft btn-primary">Primary</button>
 
       <div>
-        <ul>
-          <li>All Products</li>
-
-          <li>My Exports</li>
-
-          <li>My Imports </li>
-        </ul>
+        <NavLink to="/">All Products</NavLink>
+        <NavLink to="/signup">Signup</NavLink>
+        <NavLink to="/login">Login</NavLink>
       </div>
+
       <button className="btn btn-primary">Login</button>
     </div>
   );
