f5c958df6b36e759a7ed92e5a0c39b4698c2f746
enghelalbd <informhelal@yahoo.com>
2026-04-17 23:25:22 +0600

    Banner Added

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+
+const banners = ["/Images/1.jpg", "/Images/3.jpg"];
 
 const Banner = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [index, setIndex] = useState(0);
+
+  const nextSlide = () => {
+    setIndex((index + 1) % banners.length);
+  };
+
+  const prevSlide = () => {
+    setIndex((index - 1 + banners.length) % banners.length);
+  };
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <img
+        src={banners[index]}
+        alt="banner"
+        style={{ width: "100%", height: "auto" }}
+      />
+
+      <div style={{ marginTop: "10px" }}>
+        <button onClick={prevSlide}>Prev</button>
+        <button onClick={nextSlide}>Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
