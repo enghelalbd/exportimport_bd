3a7d05d9096ff35dc44ca8b84c26082ad4ffad05
enghelalbd <informhelal@yahoo.com>
2026-04-22 11:45:32 +0600

    Router defined

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Router } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import "./index.css";
 import App from "./App.jsx";
-import Login from "./Component/Login.jsx";
-import Register from "./Component/Register.jsx";
-import MyImports from "./Pages/MyImports.jsx";
-import AddExport from "./Pages/AddExports.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/MyExports",
-    element: <MyExports />,
-  },
-  {
-    path: "/MyImports",
-    element: <MyImports />,
-  },
-
-  {
-    path: "/AddExports",
-    element: <AddExport />,
-  },
-  {
-    path: "*",
-    element: <div>404 Not Found</div>,
-  },
-]);
+import { router } from "./routes/Routers.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -3,10 +3,42 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
-const router = createBrowserRouter([
+import Register from "../Component/Register.jsx";
+import MyImports from "../Pages/MyImports.jsx";
+import AddExport from "../Pages/AddExports.jsx";
+import MyExports from "../Pages/MyExports.jsx";
+import Login from "../Component/Login.jsx";
+import App from "../App.jsx";
+
+export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/MyExports",
+    element: <MyExports />,
+  },
+  {
+    path: "/MyImports",
+    element: <MyImports />,
+  },
+
+  {
+    path: "/AddExports",
+    element: <AddExport />,
+  },
+  {
+    path: "*",
+    element: <div>404 Not Found</div>,
   },
 ]);
 
