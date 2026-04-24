a2a74ad4838fff2654b626935d4f1ecfd400323c
enghelalbd <informhelal@yahoo.com>
2026-04-24 07:15:29 +0600

    Handel Submit function Created

--- src/Pages/MyExports.jsx
+++ src/Pages/MyExports.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
 const MyExports = () => {
+  const handelSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      productName: e.target.productName.value,
+      productImage: e.target.productImage.value,
+      price: parseFloat(e.target.price.value),
+      originCountry: e.target.originCountry.value,
+      rating: parseFloat(e.target.rating.value),
+      availableQuantity: parseInt(e.target.availableQuantity.value),
+      created_at: new Date(),
+    };
+    console.log(formData);
+  };
+
   return (
     <div>
       {" "}
-      <h1 className="text-2xl font-bold mb-5 text-center"> My Exports bd </h1>
-      <form>
+      <h1 className="text-2xl font-bold mb-5 text-center">
+        {" "}
+        Product Exports Form{" "}
+      </h1>
+      <form onSubmit={handelSubmit}>
         <div className="mb-4">
           <label
             className="block text-gray-700 font-bold mb-2"
@@ -88,13 +106,14 @@ const MyExports = () => {
             className="w-full p-2 border rounded"
           />
         </div>
-
-        <button
-          className="bg-blue-500 text-center text-white py-2 px-4 rounded mt-4 hover:bg-blue-600"
-          type="submit"
-        >
-          Add Export
-        </button>
+        <div className="flex justify-center">
+          <button
+            className="bg-blue-500 text-center text-white py-2 px-4 rounded mt-4 hover:bg-blue-600"
+            type="submit"
+          >
+            Add Export
+          </button>
+        </div>
       </form>
     </div>
   );
